6436e18fd77d8634d431f07c72f447326e67b38b
Amit Kumar Mondal <admin@amitinside.com>
2025-04-15 20:41:14 +0200

    Added Index Icon

---
+++ frontend/src/components/common/icons/index.ts
@@ -0,0 +1,31 @@
+import Chat from "./Chats";
+import Info from "./Info";
+import Switcher from "./Order";
+import Delete from "./Delete";
+import Save from "./Save";
+import Edit from "./Edit";
+import LoadingIcon from "./LoadingIcon";
+import ThumbsDown from "./ThumbsDown";
+import ThumbsUp from "./ThumbsUp";
+import Copy from "./Copy";
+import Source from "./Source";
+import Moon from "./Moon";
+import Sun from "./Sun";
+import LogoutIcon from "./LogoutIcon";
+
+export {
+    Chat,
+    Info,
+    Switcher,
+    Delete,
+    Save,
+    Edit,
+    LoadingIcon,
+    ThumbsDown,
+    ThumbsUp,
+    Copy,
+    Source,
+    Moon,
+    Sun,
+    LogoutIcon
+};
